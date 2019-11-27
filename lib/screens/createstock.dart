eaece727deadd0ad2457508024df8d98189307c0
TinchoOlivari <martin.olivari01@gmail.com>
2019-11-27 19:57:31 -0300

    Launcher icon modified

--- lib/screens/createstock.dart
+++ lib/screens/createstock.dart
@@ -48,75 +48,79 @@ class _PaginaCreaciState extends State<PaginaCreacion> {
       appBar: AppBar(
         title: Text("Agregar"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
               children: <Widget>[
-                TextField(
-                  controller: myController,
-                  maxLength: 10,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Ingresar titutlo',
-                    errorText: _validate ? 'Debes ingresar un texto' : null,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5.0),
-                ),
-                TextField(
-                  controller: myController2,
-                  maxLength: 30,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Ingresar descripcion',
-                    errorText: _validate2 ? 'Debes ingresar un texto' : null,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5.0),
-                ),
-                TextField(
-                  keyboardType: TextInputType.number,
-                  controller: myController3,
-                  maxLength: 3,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Ingresar cantidad',
-                    errorText: _validate3 ? 'Debes ingresar un numero' : null,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: MaterialButton(
-                    child: Text(
-                      'Guardar',
-                      style: TextStyle(color: Colors.white),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    TextField(
+                      controller: myController,
+                      maxLength: 10,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Ingresar titutlo',
+                        errorText: _validate ? 'Debes ingresar un texto' : null,
+                      ),
                     ),
-                    color: Colors.blue,
-                    onPressed: () {
-                      setState(() {
-                        myController.text.isEmpty
-                            ? _validate = true
-                            : _validate = false;
-                        myController2.text.isEmpty
-                            ? _validate2 = true
-                            : _validate2 = false;
-                        myController3.text.isEmpty
-                            ? _validate3 = true
-                            : _validate3 = false;
-                      });
-                      _addStock();
-                    },
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    ),
+                    TextField(
+                      controller: myController2,
+                      maxLength: 30,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Ingresar descripcion',
+                        errorText: _validate2 ? 'Debes ingresar un texto' : null,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.number,
+                      controller: myController3,
+                      maxLength: 3,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Ingresar cantidad',
+                        errorText: _validate3 ? 'Debes ingresar un numero' : null,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: MaterialButton(
+                        child: Text(
+                          'Guardar',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: Colors.blue,
+                        onPressed: () {
+                          setState(() {
+                            myController.text.isEmpty
+                                ? _validate = true
+                                : _validate = false;
+                            myController2.text.isEmpty
+                                ? _validate2 = true
+                                : _validate2 = false;
+                            myController3.text.isEmpty
+                                ? _validate3 = true
+                                : _validate3 = false;
+                          });
+                          _addStock();
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
